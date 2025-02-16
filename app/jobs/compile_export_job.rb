5ac437f7590df0b1944c837a93071a0c2ea067c1
Bobby Feeney <bfeeney@covermymeds.com>
2025-02-16 17:37:33 -0500

    blah

--- app/jobs/compile_export_job.rb
+++ app/jobs/compile_export_job.rb
@@ -2,10 +2,10 @@ class CompileExportJob < ApplicationJob
   queue_as :default
 
   def perform(export_id)
-    export = PrintExport.find(export_id)
-    io = PrintJobCompiler.generate(export.job_orders)
-    export.pdf.attach(io: io.read)
-    export.finished = true
-    export.save
+    # export = PrintExport.find(export_id)
+    # io = PrintJobCompiler.generate(export.job_orders)
+    # export.pdf.attach(io: io.read)
+    # export.finished = true
+    # export.save
   end
 end
