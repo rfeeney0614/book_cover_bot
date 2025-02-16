a2597fc4973981dd874fb32af35fa9e4b099b3e2
Bobby Feeney <bfeeney@covermymeds.com>
2025-02-16 18:03:21 -0500

    DateTime

--- app/jobs/compile_export_job.rb
+++ app/jobs/compile_export_job.rb
@@ -4,7 +4,7 @@ class CompileExportJob < ApplicationJob
   def perform(export_id)
     export = PrintExport.find(export_id)
     io = PrintJobCompiler.generate(export.job_orders)
-    export.pdf.attach(io: io.read, filename: "export_#{datetime.now}")
+    export.pdf.attach(io: io.read, filename: "export_#{DateTime.now}")
     export.finished = true
     export.save
   end
