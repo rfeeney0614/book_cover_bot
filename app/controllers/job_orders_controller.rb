8724db326d39c81b097dc965527e2af712aab728
Bobby Feeney <bfeeney@covermymeds.com>
2025-02-16 17:42:21 -0500

    ksajdbfnskajdbf

--- app/controllers/job_orders_controller.rb
+++ app/controllers/job_orders_controller.rb
@@ -7,7 +7,8 @@ class JobOrdersController < ApplicationController
   def export
     @export = PrintExport.create
     @export.job_orders.append(JobOrder.all)
-    CompileExportJob.perform_later(export.id)
+    @export.save
+    # CompileExportJob.perform_later(export.id)
     respond_to do |format|
        format.json { render :json => {:jobId => @export.id} }
     end
