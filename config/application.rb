4c6fbfb2d4e2da0959c39f7cad66c5cf4e44e51f
Bobby Feeney <bfeeney@covermymeds.com>
2025-02-16 18:28:07 -0500

    no auth on mission control

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module BookCoverBot
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    config.mission_control.jobs.http_basic_auth_enabled = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
